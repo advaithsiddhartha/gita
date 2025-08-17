786f93e72a6643d8b63ffaf3fffc98dbdcda0022
advaithsiddhartha <142304435+advaithsiddhartha@users.noreply.github.com>
2025-08-18 01:18:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def load_resources():
     index = faiss.read_index("gita_index.faiss")
     with open("gita_verses.json", "r", encoding="utf-8") as f:
         data = json.load(f)
-    model = SentenceTransformer("paraphrase-MiniLM-L6-v2")  # light & stable
+    model = SentenceTransformer("sentence-transformers/paraphrase-MiniLM-L6-v2")
     return index, data, model
 
 index, data, model = load_resources()
